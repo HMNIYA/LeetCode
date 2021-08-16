0f6641a79f6d614a828edd19b4d0fdfa28e8c598
YegorAlexandrov <farlyteem@gmail.com>
2021-08-16 23:35:01 +0600

    Добавение новых решений задач с LeetCode.

---
+++ src/com/company/SIngleNumber3.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class SIngleNumber3 {
+    public static void main(String[] args) {
+        int[] nums = {1,2,1,3,2,5};
+        Set<Integer> set = new HashSet();
+        for(int num : nums){ //Set: 3,5
+            if(!set.contains(num)){
+                set.add(num);
+            }
+            else{
+                set.remove(num);
+            }
+        }
+        int[] result = new int[2];
+        int i = 0;
+        for(int num : set){
+            result[i] = num;
+            i++;
+        }
+        //return result;
+    }
+}
---
+++ src/com/company/SingleNumber2.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class SingleNumber2 {
+    public static void main(String[] args) {
+        int[] nums = {3, 3, 3, 1};
+        int p = 0;
+        int q = 0;
+        for(int i = 0; i<nums.length; i++){
+            p = q & (p ^ nums[i]);
+            q = p | (q ^ nums[i]);
+        }
+        //return q;
+    }
+}
